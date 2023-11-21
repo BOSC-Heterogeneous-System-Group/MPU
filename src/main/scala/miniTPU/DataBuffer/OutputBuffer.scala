9546c0efbcf034753b7b50363e8082adc8f93527
hsp <husipeng24@qq.com>
2023-11-21 17:15:46 +0800

    Clean up redundant control logic and rename signals to improve code readability

--- src/main/scala/miniTPU/DataBuffer/OutputBuffer.scala
+++ src/main/scala/miniTPU/DataBuffer/OutputBuffer.scala
@@ -3,30 +3,46 @@ package miniTPU.DataBuffer
 import chisel3._
 import chisel3.util._
 
+
+// TODO: fusion vertical InputBuffer and OutputBuffer as one module?
 class OutputBuffer(val C_WIDTH: Int, val QUEUE_NUM: Int, val QUEUE_LEN: Int) extends Module {
   val io = IO(new Bundle {
-    val ctrl_valid    = Input(Bool())
-    val ctrl_ready    = Input(Bool())
-    val ctrl_ob_ready = Input(Bool())
+    val ctrl_ob_data_in = Input(Bool())
     val data_in = Input(Vec(QUEUE_NUM, SInt(C_WIDTH.W)))
 
-    val data_out = Output(Vec(QUEUE_NUM, SInt(C_WIDTH.W)))
-    val all_empty  = Output(Bool())
+    val data_out = DecoupledIO(Vec(QUEUE_NUM, SInt(C_WIDTH.W)))
+    val ob_empty  = Output(Bool())
   })
 
   val data_queue = Seq.fill(QUEUE_NUM)(Module(new SyncFIFO(C_WIDTH, QUEUE_LEN)))
 
-  for (i <- 0 until QUEUE_NUM) {
-    data_queue(i).io.enq := io.ctrl_ob_ready
-    data_queue(i).io.deq := io.ctrl_valid && io.ctrl_ready
-    data_queue(i).io.enqData := io.data_in(i)
-    io.data_out(i) := data_queue(i).io.deqData
+  // io.ctrl_ob_data_in is a pulse signal, we need to extend it
+  val count = RegInit(0.U(log2Ceil(QUEUE_LEN)))
+  val isDataIn = RegInit(false.B)
+  val stop = count === (QUEUE_NUM - 2).U
+  when(io.ctrl_ob_data_in) {
+    isDataIn := true.B
+  }.elsewhen(stop) {
+    isDataIn := false.B
+  }
+  when(isDataIn) {
+    when(!stop) {
+      count := count + 1.U
+    }.otherwise {
+      count := 0.U
+    }
   }
 
-  val allFull = WireInit(false.B)
   val allEmpty = WireInit(false.B)
-  allFull := data_queue.map(_.io.full).reduce(_ & _)
   allEmpty := data_queue.map(_.io.empty).reduce(_ & _)
 
-  io.all_empty := allEmpty
+  for (i <- 0 until QUEUE_NUM) {
+    data_queue(i).io.enq := io.ctrl_ob_data_in || isDataIn
+    data_queue(i).io.deq := io.data_out.ready && !allEmpty
+    data_queue(i).io.enqData := io.data_in(i)
+    io.data_out.bits(i) := data_queue(i).io.deqData
+  }
+
+  io.data_out.valid := !allEmpty
+  io.ob_empty := allEmpty
 }
--- src/main/scala/miniTPU/top.scala
+++ src/main/scala/miniTPU/top.scala
@@ -1,7 +1,6 @@
 package miniTPU
 
 import chisel3._
-import chisel3.stage._
 import chisel3.util._
 import miniTPU.DataBuffer._
 import miniTPU.SystolicArray._
@@ -17,90 +16,49 @@ class miniTPUOutput(val IN_WIDTH: Int, val C_WIDTH: Int, val SA_ROWS: Int, val S
 }
 
 class miniTPUIO (val IN_WIDTH: Int, val C_WIDTH: Int, val SA_ROWS: Int, val SA_COLS: Int) extends Bundle {
-
-
   val in = Flipped(DecoupledIO(new miniTPUInput(IN_WIDTH, C_WIDTH, SA_ROWS, SA_COLS)))
-
   val out = DecoupledIO(new miniTPUOutput(IN_WIDTH, C_WIDTH, SA_ROWS, SA_COLS))
-}
-class top_wrapper (val IN_WIDTH: Int, val C_WIDTH: Int, val SA_ROWS: Int, val SA_COLS: Int) extends Module {
-  val io = IO(new Bundle {
-    val tpuIO = new miniTPUIO(IN_WIDTH,C_WIDTH,SA_ROWS,SA_COLS)
-  })
-
-  val top = Module(new top(IN_WIDTH,C_WIDTH,SA_ROWS,SA_COLS))
-
-  top.io.in_valid := io.tpuIO.in.valid
-  top.io.in_ready := io.tpuIO.out.ready
-  top.io.in_a     := io.tpuIO.in.bits.in_a
-  top.io.in_b     := io.tpuIO.in.bits.in_b
-  top.io.in_c     := io.tpuIO.in.bits.in_c
-
-  io.tpuIO.out.valid      := top.io.out_valid
-  io.tpuIO.in.ready       := top.io.out_ready
-  io.tpuIO.out.bits.out_c := top.io.out_c
-
 }
 
 class top (val IN_WIDTH: Int, val C_WIDTH: Int, val SA_ROWS: Int, val SA_COLS: Int) extends Module {
   val io = IO(new Bundle {
-    val in_valid  = Input(Bool())
-    val in_ready  = Input(Bool())
-    val in_a      = Input(Vec(SA_ROWS, SInt(IN_WIDTH.W)))
-    val in_b      = Input(Vec(SA_COLS, SInt(IN_WIDTH.W)))
-    val in_c      = Input(Vec(SA_COLS, SInt(C_WIDTH.W)))
-
-    val out_valid = Output(Bool())                        // TODO: decoupledIO
-    val out_ready = Output(Bool())
-    val out_c     = Output(Vec(SA_COLS, SInt(C_WIDTH.W)))
+    val start = Input(Bool())
+    val tpuIO = new miniTPUIO(IN_WIDTH, C_WIDTH, SA_ROWS, SA_COLS)
   })
 
   val sa = Module(new SystolicArray(IN_WIDTH,C_WIDTH,SA_ROWS,SA_COLS))
   val controller = Module(new Controller(SA_ROWS, SA_COLS))
-  // horizontal and vertical data buffer
-  val inBuffer_h   = Module(new InputBuffer(IN_WIDTH, SA_COLS, SA_ROWS))
+  val inBuffer_h   = Module(new InputBuffer(IN_WIDTH, SA_ROWS, SA_COLS))  // horizontal and vertical data buffer
   val inBuffer_v  = Module(new InputBuffer(IN_WIDTH, SA_COLS, SA_ROWS)) // TODO: add control logic to select data( B or D)
   val outBuffer  = Module(new OutputBuffer(C_WIDTH, SA_COLS, SA_ROWS))
 
-  val ready = RegInit(false.B)
+  inBuffer_h.io.data_in.valid := io.tpuIO.in.valid
+  inBuffer_h.io.data_in.bits := io.tpuIO.in.bits.in_a
+  io.tpuIO.in.ready := inBuffer_h.io.data_in.ready
+  inBuffer_h.io.ctrl_ib_data_in := controller.io.ctrl_ib_data_in
+  inBuffer_h.io.ctrl_ib_data_out := controller.io.ctrl_ib_data_out
 
-  inBuffer_h.io.data_in := io.in_a
-  inBuffer_v.io.data_in := io.in_b
+  inBuffer_v.io.data_in.valid := io.tpuIO.in.valid
+  inBuffer_v.io.data_in.bits := io.tpuIO.in.bits.in_b
+  io.tpuIO.in.ready := inBuffer_v.io.data_in.ready
+  inBuffer_v.io.ctrl_ib_data_in := controller.io.ctrl_ib_data_in
+  inBuffer_v.io.ctrl_ib_data_out := controller.io.ctrl_ib_data_out
+
+  io.tpuIO.out.valid := outBuffer.io.data_out.valid
+  io.tpuIO.out.bits.out_c := outBuffer.io.data_out.bits
+  outBuffer.io.data_out.ready := io.tpuIO.out.ready
+  outBuffer.io.ctrl_ob_data_in := controller.io.ctrl_ob_data_in
 
   sa.io.in_a := inBuffer_h.io.data_out
   sa.io.in_b := inBuffer_v.io.data_out
-  sa.io.in_c := io.in_c                 // TODO: preload in_c as bias
-
-  ready := io.in_ready
-
-
-  for (c <- 0 until SA_COLS) {
-    sa.io.in_control(c).done := controller.io.ctrl_cal_done
-  }
-  controller.io.ctrl_cal_start := inBuffer_h.io.cal_start & inBuffer_v.io.cal_start
-  controller.io.ctrl_in_done := inBuffer_h.io.data_in_done & inBuffer_v.io.data_in_done
-  controller.io.ctrl_ob_empty := outBuffer.io.all_empty
-  controller.io.ctrl_ibh_full := inBuffer_h.io.all_full
-  controller.io.ctrl_ibv_full := inBuffer_v.io.all_full
-  controller.io.ctrl_ibh_empty := inBuffer_h.io.all_empty
-  controller.io.ctrl_ibv_empty := inBuffer_v.io.all_empty
-  controller.io.ctrl_pre_valid := io.in_valid
-  controller.io.ctrl_post_ready := io.in_ready
-
-
-  inBuffer_h.io.ctrl_data_valid := io.in_valid
-  inBuffer_v.io.ctrl_data_valid := io.in_valid
-  inBuffer_h.io.ctrl_data_in := controller.io.ctrl_ib_data_in
-  inBuffer_h.io.ctrl_data_out := controller.io.ctrl_ib_data_out
-  inBuffer_v.io.ctrl_data_in := controller.io.ctrl_ib_data_in
-  inBuffer_v.io.ctrl_data_out := controller.io.ctrl_ib_data_out
-
-  outBuffer.io.ctrl_valid := controller.io.ctrl_post_valid
-  outBuffer.io.ctrl_ready := controller.io.ctrl_post_ready
-  outBuffer.io.ctrl_ob_ready := controller.io.ctrl_ob_ready
+  sa.io.in_c := io.tpuIO.in.bits.in_c  // TODO: preload in_c as bias
   outBuffer.io.data_in := sa.io.out_c
+  sa.io.in_control.foreach(_.ctrl_send_data := controller.io.ctrl_sa_send_data)
 
-  io.out_valid := controller.io.ctrl_post_valid
-  io.out_ready := controller.io.ctrl_pre_ready
-  io.out_c := outBuffer.io.data_out
+  controller.io.start := io.start
+  controller.io.ibh_data_in_done := inBuffer_h.io.ib_data_in_done
+  controller.io.ibh_empty := inBuffer_h.io.ib_empty
+  controller.io.ibv_data_in_done := inBuffer_v.io.ib_data_in_done
+  controller.io.ibv_empty := inBuffer_v.io.ib_empty
+  controller.io.ob_empty := outBuffer.io.ob_empty
 }
